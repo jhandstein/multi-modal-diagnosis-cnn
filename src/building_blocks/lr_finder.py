a9e9211c98c0b0cb704cc6adf838e9c84f7f3f47
Julius Handstein <julius.handstein@web.de>
2025-02-25 17:18:37 +0100

    trying to implement a way to use 1cylce learning rate scheduler

--- src/building_blocks/lr_finder.py
+++ src/building_blocks/lr_finder.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import lightning as L
 
 from lightning.pytorch.tuner import Tuner
+from pytorch_lightning.callbacks import LearningRateMonitor
+import torch
 
 from src.building_blocks.model_factory import ModelFactory
 from src.building_blocks.lightning_wrapper import LightningWrapperCnn
@@ -10,6 +12,7 @@ from src.data_management.data_loader import prepare_standard_data_loaders
 from src.data_management.data_set import DataSetConfig
 from src.data_management.data_set_factory import DataSetFactory
 from src.utils.config import AGE_SEX_BALANCED_10K_PATH, PLOTS_PATH, FeatureMapType
+from utils.cuda_utils import calculate_model_size
 
 
 def estimate_initial_learning_rate():
@@ -105,3 +108,85 @@ def plot_lr_finder_results(lr_finder, model_name: str):
     save_path = PLOTS_PATH / "lr_finder" / f"{model_name}_lr_finder_results.png"
     plt.savefig(save_path)
     plt.close()
+
+
+def test_lr_monitor_one_cylce_policy():
+    """Test the OneCycleLR policy"""
+
+    # https://github.com/Lightning-AI/pytorch-lightning/discussions/13236
+    # https://github.com/Lightning-AI/pytorch-lightning/discussions/9601
+
+    class TestWrapper(LightningWrapperCnn):
+
+        def configure_optimizers(self):
+            optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+            lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(
+                    optimizer,
+                    max_lr=0.1,
+                    steps_per_epoch=64,
+                    epochs=10,
+                    three_phase=True
+                )
+            return [optimizer], [lr_scheduler]
+        
+    # Set parameters for training
+    task = "regression" # "classification" "regression"
+    dim = "3D"
+    feature_map = FeatureMapType.GM
+    target = "sex" if task == "classification" else "age"
+    model_type = "ResNet18" # "ResNet18" "ConvBranch"
+
+    num_gpus = torch.cuda.device_count()
+    batch_size = 64 if dim == "2D" else 2 # should be maximum val_set size / num_gpus?
+    learning_rate = 1e-3
+    experiment_notes = {"notes": f"ResNet183D with accumulate batches=8, batch_size=2. Running classification with Adam default LR. Scaling of 0.5 for image inputs."}
+
+    print_collection_dict = {
+        "Task": task,
+        "Batch Size": batch_size,
+        "Num GPUs": num_gpus,
+        "Learning Rate": learning_rate,
+        "Experiment Notes": experiment_notes,
+    }
+
+    # Prepare data sets and loaders
+    ds_config = DataSetConfig(
+        feature_map=feature_map,
+        target=target,
+        middle_slice=True if dim == "2D" else False,
+    )
+    data_split = DataSplitFile(AGE_SEX_BALANCED_10K_PATH).load_data_splits_from_file()
+    
+    train_set, val_set, test_set = DataSetFactory(
+        data_split["train"], data_split["val"], data_split["test"], ds_config
+    ).create_data_sets()
+
+    train_loader = prepare_standard_data_loaders(
+        train_set, batch_size=batch_size
+    )
+    val_loader = prepare_standard_data_loaders(val_set, batch_size=batch_size)
+
+    # Setup model
+    if model_type == "ConvBranch":
+        model = ModelFactory(task=task, dim=dim).create_conv_branch(input_shape=train_set.data_shape)
+    elif model_type == "ResNet18":
+        model = ModelFactory(task=task, dim=dim).create_resnet18(in_channels=1)
+    else:
+        raise ValueError("Model type not supported. Check the model_type argument.")
+
+    print_collection_dict["Model Size"] = calculate_model_size(model)
+
+    wrapper = TestWrapper(
+        model=model,
+        task=task,
+        learning_rate=learning_rate
+    )
+
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+    trainer = L.Trainer(
+        default_root_dir="one_cycle_test",
+        max_epochs=10,
+        callbacks=[lr_monitor]
+    )
+    trainer.fit(wrapper, train_loader, val_loader)
+    breakpoint()
